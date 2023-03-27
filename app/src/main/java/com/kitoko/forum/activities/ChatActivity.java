3b43be9d5ac0dee73eade2bc89a188784935122a
pbokolo <dev.pbokolo@gmail.com>
2023-03-27 19:30:05 +0200

    Project initialization, Firebase config and auth implementation using firebaseui

---
+++ app/src/main/java/com/kitoko/forum/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.kitoko.forum.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kitoko.forum.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
---
+++ app/src/main/java/com/kitoko/forum/activities/MainActivity.java
@@ -0,0 +1,77 @@
+package com.kitoko.forum.activities;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.kitoko.forum.R;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            (result) -> onSignInResult(result));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startChat();
+        } else {
+            startSignIn();
+        }
+    }
+
+    private void startChat(){
+        startActivity(new Intent(this, ChatActivity.class));
+    }
+    private void startSignIn() {
+        Intent signInIntent =
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.PhoneBuilder().build()))
+                        .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+
+        if (result.getResultCode() == RESULT_OK) {
+            // Succes des l'opération
+            startChat();
+            finish();
+        } else {
+            // Echec de l'opération
+            if (response == null) {
+                // L'utilisateur a appuyé le boutton retour
+                Toast.makeText(this, "Annulée", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // Pas de connexion Internet
+            if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                Toast.makeText(this, "Pas de connexion Internet", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // Autre chose
+            Toast.makeText(this, "Erreur inconnue", Toast.LENGTH_LONG).show();
+        }
+    }
+}
