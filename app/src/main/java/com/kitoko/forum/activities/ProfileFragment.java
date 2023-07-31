1f0b178b9adc5547d8e8f9975bcadcd1f19af908
pbokolo <dev.pbokolo@gmail.com>
2023-07-31 14:11:40 +0200

    Choosing an image and rendering it in the imageview

--- app/src/main/java/com/kitoko/forum/activities/ProfileFragment.java
+++ app/src/main/java/com/kitoko/forum/activities/ProfileFragment.java
@@ -1,12 +1,19 @@
 package com.kitoko.forum.activities;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +21,30 @@ import android.view.ViewGroup;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import android.Manifest;
 import com.kitoko.forum.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
+    //The code for image choosing intent
+    private static final int RC_CHOOSE_IMG = 1000;
     private FragmentProfileBinding vBinder;
 
     public ProfileFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri selectedImg;
+        //Checks if the request code is the one of choosing image and if everything went well
+        if(requestCode == RC_CHOOSE_IMG && resultCode == RESULT_OK) {
+            if(data != null){
+                selectedImg = data.getData();
+                renderImage(selectedImg);
+            }
+        }
     }
 
     @Override
@@ -45,20 +68,56 @@ public class ProfileFragment extends Fragment {
         String username = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
         vBinder.usrnmLbl.setText(username);
         vBinder.phoneNumberLbl.setText(phone);
-        Glide.with(getContext())
-                .load("https://images.freeimages.com/images/large-previews/f98/black-coffee-1185883.jpg")
-                .apply(RequestOptions.circleCropTransform()).into(vBinder.profileImage);
+
+        renderImage(Uri.parse("https://images.freeimages.com/images/large-previews/f98/black-coffee-1185883.jpg"));
 
         vBinder.usrnmLbl.setOnClickListener(v -> Navigation.findNavController(v)
                 .navigate(ProfileFragmentDirections.profile2update(UpdateBtmSheet.USERNAME_TEXT)));
-        /*vBinder.phoneNumberLbl.setOnClickListener(v -> Navigation.findNavController(v)
-                .navigate(ProfileFragmentDirections.profile2update(UpdateBtmSheet.PHONE_TEXT)));*/
+
+        vBinder.profileImage.setOnClickListener(v -> {
+            if(isStoragePermissionGranted()){
+                startActivityForResult(new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI),
+                        RC_CHOOSE_IMG);
+            }else{
+                requestStoragePermission();
+            }
+        });
 
         vBinder.signoutBtn.setOnClickListener(v ->
             FirebaseAuth.getInstance().signOut()
         );
 
         vBinder.delAccBtn.setOnClickListener(v -> FirebaseAuth.getInstance().getCurrentUser().delete());
+
+    }
+
+    private void renderImage(Uri uri){
+        Glide.with(getContext())
+                .load(uri)
+                .apply(RequestOptions.circleCropTransform()).into(vBinder.profileImage);
+    }
+
+    /**
+     * Checks if the access to local storage is granted
+     * @return A boolean
+     */
+    private boolean isStoragePermissionGranted(){
+
+        return ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+
+
+    }
+
+    /**
+     * Displays a prompt to request for permission
+     */
+    private void requestStoragePermission() {
+
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
 
     }
 }
