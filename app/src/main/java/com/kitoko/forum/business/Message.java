de8be8e65537b02b5aa40ed3d4e3fb3304ed6ad1
pbokolo <dev.pbokolo@gmail.com>
2023-08-04 11:37:42 +0200

    Setting up the basic business with 2 classes: Subject and Message

---
+++ app/src/main/java/com/kitoko/forum/business/Message.java
@@ -0,0 +1,36 @@
+package com.kitoko.forum.business;
+
+import java.util.UUID;
+
+public class Message {
+
+    private String uid, author, text;
+
+    public Message(){
+        this.uid = UUID.randomUUID().toString();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ app/src/main/java/com/kitoko/forum/business/Subject.java
@@ -0,0 +1,45 @@
+package com.kitoko.forum.business;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Subject {
+    private String uid, title, creator;
+    private Date dateCreated;
+
+    public Subject(){
+        this.uid = UUID.randomUUID().toString();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
